695783e1efe030c0f1da8f83df650e357725a9e5
Andreas Reschenhofer <andreas-reschenhofer@gmx.at>
2016-11-24 16:02:53 +0100

    Add REST Api samples

---
+++ Boerse/BoersenApplication.cs
@@ -0,0 +1,45 @@
+﻿using Grapevine.Interfaces.Server;
+using Grapevine.Server;
+using Grapevine.Server.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boerse
+{
+	class BoersenApplication
+	{
+		static void Main(string[] args)
+		{
+			Debug.WriteLine("Started!");
+			using (var server = new RestServer())
+			{
+				server.LogToConsole().Start();
+				Console.ReadLine();
+				server.Stop();
+			}
+		}
+	}
+
+	[RestResource]
+	public class TestResource
+	{
+		[RestRoute(HttpMethod = Grapevine.Shared.HttpMethod.GET, PathInfo = "/repeat")]
+		public IHttpContext RepeatMe(IHttpContext context)
+		{
+			var word = context.Request.QueryString["word"] ?? "what?";
+			context.Response.SendResponse(word);
+			return context;
+		}
+
+		[RestRoute]
+		public IHttpContext HelloWord(IHttpContext context)
+		{
+			context.Response.SendResponse("Hello World!");
+			return context;
+		}
+	}
+}
