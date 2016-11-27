85a581e1d945eb699126690ef5bb241e4f58bc5d
Mike Thomas <fhs36415@fh-salzburg.ac.at>
2016-11-27 16:45:29 +0100

    GUI + Logic of creating Customers

--- Stock Application/MainView.cs
+++ Stock Application/MainView.cs
@@ -1,24 +1,172 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using System.Threading;
+using System.ComponentModel;
+using Telerik.WinControls.UI;
 
 namespace Stock_Application
 {
     public partial class MainView : Telerik.WinControls.UI.RadForm
     {
+
+        /// <summary>
+        /// List of customer objects which are available during runtime
+        /// This list is loaded and saved at server´s DB for persistency
+        /// </summary>
+        public BindingList<Customer> LstCustomers = new BindingList<Customer>();
+
         public MainView()
         {
             InitializeComponent();
+            initializeRuntimeData();
         }
-
-        private void MainView_Load(object sender, EventArgs e)
+       
+        /// <summary>
+        /// Sets up the runtime data and configuration
+        /// </summary>
+        private void initializeRuntimeData()
         {
-
+            LstCustomers = loadCustomersFromDB();
+            
+            setDataBindings();
         }
+
+        /// <summary>
+        /// Sets inital databindings for telerik controls
+        /// </summary>
+        private void setDataBindings()
+        {
+            grdGridCustomers.DataSource = LstCustomers;            
+        }
+
+        /// <summary>
+        /// Loads customer information from server´s DB and stores it in runtime List<Customer>
+        /// </summary>
+        /// <returns></returns>
+        private BindingList<Customer> loadCustomersFromDB()
+        {
+            BindingList<Customer> tmpCusList = new BindingList<Customer>();
+
+            try
+            {
+                var db = db_connection.customerTable.Find(new BsonDocument()).ToList();
+
+                //getting values from DB to internal format
+                foreach (var item in db)
+                {
+                    Customer tmpCus = new Customer(item.GetElement("Firstname").Value.ToString(), item.GetElement("Lastname").Value.ToString(), Double.Parse(item.GetElement("Equity").Value.ToString(), System.Globalization.NumberStyles.Any), item.GetElement("GUID").Value.ToString());
+                    tmpCusList.Add(tmpCus);
+                }
+                return tmpCusList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading customer data from database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Debug.Print(ex.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Creates a customer object with the given user input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateCustomer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //simple check implementation -> should be checked for telerik controls error provider active but no idea how to do this in winforms (Telerik wpf controls better for this)
+                //manual check with regex could be implemented here for validating the input in greater detail but no need for this lab excercise
+                if (Double.Parse(txtAmount.Value.ToString(), System.Globalization.NumberStyles.Any) /*removes space and currency sign*/ >= 0 && txtFirstName.Value.ToString() != string.Empty && txtLastName.Value.ToString() != string.Empty)
+                {
+                    //creating customer object with user input
+                    Customer tmpCus = new Customer(txtFirstName.Value.ToString(), txtLastName.Value.ToString(), Double.Parse(txtAmount.Value.ToString(), System.Globalization.NumberStyles.Any), string.Empty);
+
+                    //adding customer object to DB
+                    try
+                    {
+                        addCustomerTuple(tmpCus);
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        MessageBox.Show("Unable to insert tuple into DB.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    LstCustomers.Add(tmpCus);
+
+                    //Reset the text of inputfields
+                    txtAmount.ResetText();
+                    txtFirstName.ResetText();
+                    txtLastName.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Input for user creation does not fit the requirements.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Adds a given customer object as tuple to DB
+        /// </summary>
+        private async void addCustomerTuple(Customer tmpCusObject)
+        {
+            try
+            {
+                BsonDocument newEntry = new BsonDocument
+            {
+                {"GUID", tmpCusObject.GUID},
+                {"Firstname", tmpCusObject.Firstname},
+                {"Lastname", tmpCusObject.Lastname },
+                {"Equity", tmpCusObject.Equity }
+            };
+                await db_connection.customerTable.InsertOneAsync(newEntry);
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Deletes a customer from the database when the entry of datagrid is delted with DELETE key
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void grdGridCustomers_RowsChanging(object sender, Telerik.WinControls.UI.GridViewCollectionChangingEventArgs e)
+        {
+            if (e.Action == Telerik.WinControls.Data.NotifyCollectionChangedAction.Remove)
+            {
+                try
+                {
+                    //get ID for tupel to delete from database as well
+                    string tmpGuid = grdGridCustomers.CurrentRow.Cells[0].Value.ToString();
+
+                    //delete tupel from database
+                    var filter = Builders<BsonDocument>.Filter.Eq("GUID", tmpGuid);
+                    db_connection.customerTable.DeleteOne(filter);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unable to delete entry from database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Debug.Print(ex.Message);
+                }
+            }
+        }
+
+
+
+
     }
 }
---
+++ Stock Application/db_connection.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Application
+{
+    /// <summary>
+    /// Class handels the connection to the MongoDB instance running on a Win10 host at Amazon cloud server
+    /// </summary>
+    public static class db_connection
+    {
+        private static String connectionString = "mongodb://ec2-35-164-218-97.us-west-2.compute.amazonaws.com";
+        private static MongoClient Client = new MongoDB.Driver.MongoClient(connectionString);
+        private static IMongoDatabase dbtemp = Client.GetDatabase("ClientDBS");
+        public static IMongoCollection<BsonDocument> customerTable = dbtemp.GetCollection<BsonDocument>("CustomerTBL");
+    }
+}
